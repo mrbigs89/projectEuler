677243def5a46af5da9b78ae9abdcf2d13e7b7cf
mrbigs89 <mrbigssimone@gmail.com>
2015-02-21 16:17:57 +0100

    Completed problem 28

---
+++ src/main/java/it/mrbigs/Problem28.java
@@ -0,0 +1,26 @@
+package it.mrbigs;
+
+/**
+ * Created by Simone on 21/02/2015.
+ */
+public class Problem28 implements Problem {
+
+    private static final long LENGTH = 1001 * 1001;
+//    private static final long LENGTH = 25;
+
+    @Override
+    public long solve() {
+        long sum = 0;
+        long interval = 2;
+        int cornerCounter = -1;
+        for (long i = 1; i <= LENGTH; i += interval) {
+            sum += i;
+            cornerCounter++;
+            if (cornerCounter == 4) {
+                cornerCounter = 0;
+                interval +=2;
+            }
+        }
+        return sum;
+    }
+}
