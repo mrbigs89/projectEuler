0f445ff964cd14e288e7b0df6b56555f10c5cb4a
mrbigs89 <mrbigssimone@gmail.com>
2015-03-09 20:34:03 +0100

    Completed problem 35

---
+++ src/main/java/it/mrbigs/Problem35.java
@@ -0,0 +1,43 @@
+package it.mrbigs;
+
+import java.math.BigInteger;
+import java.util.*;
+
+import static it.mrbigs.util.Common.getDigits;
+import static it.mrbigs.util.Common.isPrime;
+
+/**
+ * Created by Simone on 08/03/2015.
+ */
+public class Problem35 implements Problem {
+
+    private static final long LIMIT = 1000000;
+
+    @Override
+    public BigInteger solve() {
+        Set<Long> primes = new HashSet<>();
+        for (long i = 0; i < LIMIT; i++) {
+            if (!primes.contains(i) && isPrime(i)) {
+                boolean allShiftsPrime = true;
+                List<Integer> digits = getDigits(i);
+                List<Long> shifts = new LinkedList<>();
+                for (int offset = 0; offset < digits.size(); offset++) {
+                    long number = 0;
+                    for (int j = 0; j < digits.size(); j++) {
+                        number += Math.pow(10, j) * digits.get((offset + j) % digits.size());
+                    }
+                    if (!isPrime(number)) {
+                        allShiftsPrime = false;
+                        break;
+                    }
+                    shifts.add(number);
+                }
+                if (allShiftsPrime) {
+                    primes.add(i);
+                    primes.addAll(shifts);
+                }
+            }
+        }
+        return BigInteger.valueOf(primes.size());
+    }
+}
